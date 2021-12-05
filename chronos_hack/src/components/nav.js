9334e01cdd3d61a08edd129fbcc8f69f60c9bcae
RevantArk <revantark@gmail.com>
2021-12-05 11:08:52 +0530

    update

--- chronos_hack/src/components/nav.js
+++ chronos_hack/src/components/nav.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import './navbar.css'
 import logo from '../assets/logo.png'
-import { Navbar } from "react-bootstrap";
 import './navbar.css';
-
 
 export default function NavBar() {
     return (
@@ -12,14 +10,10 @@ export default function NavBar() {
         <div className="logo">
             <img src={logo} alt="" />
             <span>Farmhouse</span>
-        <Navbar.Brand varaint="light">
-        Farmhouse
-        </Navbar.Brand>
         </div>    
         <div  className='nav-links'>
       
             <a  href="/">Home</a>
-            <a  href="#aboutUs">About FarmHouse</a>       
             <a  href="#aboutUs">About</a>       
             <a  href="/loginVendor" >Login as Vendor</a>
             <a  href="/loginFarmer" >Login as Farmer</a>
